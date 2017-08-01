50685ee94f60562ae3a877dc7320670618649001
Chris Stringham <C.J.Stringham@gmail.com>
2017-08-01 15:34:01 -0700

    updated copy files setup.

--- copyStaticAssets.js
+++ copyStaticAssets.js
@@ -1,12 +1,14 @@
 var shell = require('shelljs');
 
-// First make sure the directories exist
+// Remove all old files/directories if they exist
+shell.rm('-rf', 'dist/*');
+shell.rm('-rf', 'dist/public');
+
+// Create the directory
 shell.mkdir('dist/public/');
-shell.mkdir('dist/public/js');
-shell.mkdir('dist/public/fonts');
-shell.mkdir('dist/public/images');
 
 // Copy files to the directories.
 shell.cp('-R', 'src/public/js', 'dist/public/');
 shell.cp('-R', 'src/public/fonts', 'dist/public/');
 shell.cp('-R', 'src/public/images', 'dist/public/');
+shell.cp('src/index.html', 'dist/');
