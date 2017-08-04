baf35e7a65143d4afd0bc6272d202555764135d7
Chris Stringham <C.J.Stringham@gmail.com>
2017-08-03 21:34:53 -0700

    working router and queries maps api

--- server.go
+++ server.go
@@ -1,11 +1,17 @@
 package main
 
 import (
+	"encoding/json"
 	"flag"
 	"fmt"
 	"log"
 	"net/http"
 	"os/exec"
+	"time"
+
+	"github.com/gorilla/mux"
+	"github.com/kr/pretty"
+	"googlemaps.github.io/maps"
 )
 
 func main() {
@@ -15,6 +21,7 @@ func main() {
 	serve := flag.String("serve", "prod", "Serve production files as default.")
 	flag.Parse()
 
+	// If the serve flag is prod it will start npm
 	if *serve == "prod" {
 		cmd := exec.Command("npm", "start-prod")
 		err := cmd.Start()
@@ -25,15 +32,49 @@ func main() {
 		err = cmd.Wait()
 		log.Printf("Command finished with error: %v", err)
 	}
+
 	// handle all requests by serving a file of the same name
 	fs := http.Dir(*dir)
 	fileHandler := http.FileServer(fs)
-	http.Handle("/", fileHandler)
+
+	router := mux.NewRouter().StrictSlash(true)
+	router.HandleFunc("/query",
+		func(w http.ResponseWriter, r *http.Request) { getRestaurant(w, r) }).
+		Queries("loc", "{location}")
+	router.PathPrefix("/").Handler(fileHandler)
 
 	log.Printf("Running on port: %d\n", *port)
 
 	addr := fmt.Sprintf("127.0.0.1:%d", *port)
-	// this call blocks -- the progam runs here forever
-	err := http.ListenAndServe(addr, nil)
+	err := http.ListenAndServe(addr, router)
 	fmt.Println(err.Error())
+}
+
+func getRestaurant(w http.ResponseWriter, r *http.Request) {
+	t0 := time.Now()
+	vars := mux.Vars(r)
+
+	w.Header().Set("Content-Type", "application/json; charset=UTF-8")
+	w.WriteHeader(http.StatusOK)
+
+	c, err := maps.NewClient(maps.WithAPIKey("AIzaSyC-SZ-3fDJXC2ajIwO-PY4ypm2Y80FLF08"))
+	if err != nil {
+		log.Fatalf("fatal error: %s", err)
+	}
+	dirRequest := &maps.NearbySearchRequest{
+		Name: vars["location"],
+	}
+	// resp, _, err := c.Directions(context.Background(), dirRequest)
+	// if err != nil {
+	// 	log.Fatalf("fatal error: %s", err)
+	// }
+
+	pretty.Println(vars["location"])
+	pretty.Println(c)
+
+	log.Printf("Search took %s", time.Since(t0))
+	err = json.NewEncoder(w).Encode(dirRequest)
+	if err != nil {
+		panic(err)
+	}
 }
